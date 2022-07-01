4514e557e8f1a8f9bd50c1e2de73ebbc113a1874
Ezekiel Smith <ezesmith107@gmail.com>
2022-07-01 12:07:29 -0400

    Part 2

--- about.js
+++ about.js
@@ -5,10 +5,19 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert("The form has been successfully submitted.");
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+function compliment(event) {
+	event.preventDefault();
+
+	alert("You are full of potential!");
+}
+
+let picture = document.querySelector("img");
+picture.addEventListener("mouseover", compliment)
